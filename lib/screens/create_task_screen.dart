ba95f446929181630847f2834fcf52cd8ee85058
Aymen Bouazizi <aymendotbouazizi@gmail.com>
2024-05-19 09:27:34 +0100

    Init project

---
+++ lib/screens/create_task_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(title: const Text('Task tracker'),),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+      },child: const Icon(Icons.add),),
+    );
+  }
+}
